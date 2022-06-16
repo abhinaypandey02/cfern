553a7bbe98f0f8bd07645b5dff64beda09c1bb77
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2022-06-16 18:35:01 +0530

    hello

--- pages/index.js
+++ pages/index.js
@@ -10,38 +10,53 @@ export default function Home() {
         <meta name="description" content="hello" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body class="bg-gray-300">
-      <div class="container mx-auto my-auto p-2 bg-white flex justify-center items-center align-middle">
+      <body class="bg-gray-300 grid place-items-center h-screen">
 
-        <div class='container first mx-auto my-auto bg-amber-200 flex flex-col'>
-          <div class='container p-12 '>REGISTRATION FORM</div>
-          <div class='flex p-8 bg-blue-500'>
-          <label class="block w-1/3 p-4 bg-green-200">
-            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-              Email
+      <div class="container rounded-lg border-2 border-sky-700 mx-auto p-2 xl:w-1/2 bg-white flex justify-center items-center align-middle">
+
+        <div class='container first mx-auto my-auto flex flex-col'>
+          <div class='container my-3 px-8 pt-3 font-bold text-2xl'>About You</div>
+          <div class='mt-2 mb-5'>
+
+          <label class="block px-8 md:w-9/12">
+            <span class="after:content-['*'] after:ml-0.5 after:text-sky-700 block text-sm font-medium text-slate-700">
+              Name
             </span>
-            <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
-          </label>
-          <label class="block w-1/3 p-4 bg-green-200 ">
-            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-              Username
+            <div class='flex '>
+            <input type="email" name="email" class="mt-1 mr-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="First" />
+            <input type="email" name="email" class="mt-1 mx-2  px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Middle" />
+            <input type="email" name="email" class="mt-1 ml-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Last" />
+            </div>
+            </label>
+
+            <label class="block mt-2 px-8 sm:w-1/2">
+            <span class="after:content-['*'] after:ml-0.5 after:text-sky-700 block text-sm font-medium text-slate-700">
+              Social Security Number
             </span>
-            <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="@username" />
-          </label>
-          <label class="block w-1/3 p-4 bg-green-200">
-            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-             Password
+            <div class='flex '>
+            <input type="email" name="email" class="mt-1 mr-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="SSN" />
+            </div>
+            </label>
+
+            <label class="block mt-2 px-8 sm:w-1/3">
+            <span class="after:content-['*'] after:ml-0.5 after:text-sky-700 block text-sm font-medium text-slate-700">
+              Date-Of-Birth
             </span>
-            <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="*******" />
-          </label>
+            <div class='flex '>
+            <input type="email" name="email" class="mt-1 mr-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="YYYY-MM-DD" />
+            </div>
+            </label>
+          
           </div>
+
+        
+        </div>
         </div>
 
-      </div>
-
+        
       </body>
 
-     
+      
   
     </div>
   )
