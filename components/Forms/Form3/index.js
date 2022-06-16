1acf6dd5369a09c7c648c369a97583c14a1598b8
Abhinay Pandey <abhinaypandey02@gmail.com>
2022-06-16 18:41:13 +0530

    Forms 1.0

---
+++ components/Forms/Form3/index.js
@@ -0,0 +1,82 @@
+import styles from "../Forms.module.scss";
+
+export default function Form3({onBack,visited}){
+    const formContainerStyle=visited?styles.backAnim:styles.nextAnim;
+    return <div tabIndex={0} className={formContainerStyle}>
+        <form>
+            <div className={styles.formGroup}>
+                <div className={styles.formLabel}>
+                    Name
+                </div>
+                <div className={styles.inputGroup}>
+                    <input type={'text'} name={'first name'} placeholder={'First Name'}/>
+                    <input type={'text'} name={'middle name'} placeholder={'Middle Name'}/>
+                    <input type={'text'} name={'last name'} placeholder={'Last Name'}/>
+                </div>
+
+            </div>
+            <div className={styles.formGroup}>
+                <div className={styles.formLabel}>
+                    Social Insurance number
+                </div>
+                <div className={styles.inputGroup}>
+                    <input type={'number'} name={'first name'} placeholder={''}/>
+                </div>
+
+            </div>
+            <div className={styles.formGroup}>
+                <div className={styles.formLabel}>
+                    Date of birth
+                </div>
+                <div className={styles.inputGroup}>
+                    <input type={'date'} name={'date of birth'}/>
+                </div>
+
+            </div>
+            <div className={styles.formGroup}>
+                <div className={styles.formLabel}>
+                    Preferred language
+                </div>
+                <div className={styles.inputGroup}>
+                    <select style={{width:164}}>
+                        <option>
+
+                        </option>
+                        <option>
+                            English
+                        </option>
+                        <option>
+                            French
+                        </option>
+                    </select>
+                </div>
+
+            </div>
+            <div className={styles.formGroup}>
+                <div className={styles.formLabel}>
+                    Is this return for a deceased person?
+                </div>
+                <div className={styles.inputGroup}>
+                    <select style={{width:70}}>
+                        <option>
+
+                        </option>
+                        <option>
+                            Yes
+                        </option>
+                        <option>
+                            No
+                        </option>
+                    </select>
+                </div>
+
+            </div>
+            <div className={styles.formGroup}>
+                <div className={styles.inputGroup}>
+                    <button className={styles.btnBack} onClick={onBack}>Back</button>
+                </div>
+
+            </div>
+        </form>
+    </div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,35 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
+import {useState} from "react";
+import Form1 from "../components/Forms/Form1";
+import Form2 from "../components/Forms/Form2";
+import Form3 from "../components/Forms/Form3";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Cfern</title>
-        <meta name="description" content="hello" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body class="bg-gray-300">
-      <div class="container mx-auto my-auto p-2 bg-white flex justify-center items-center align-middle">
+    const [currentForm, setCurrentForm] = useState(0);
+    const [prevForm, setPrevForm] = useState(0);
+    const onNext = () => setCurrentForm(old => {
+        setPrevForm(old);
+        return old + 1
+    });
+    const onBack = () => setCurrentForm(old => {
+        setPrevForm(old);
+        return old - 1
+    });
+    const FORMS = [
+        <Form1 onNext={onNext} key={0} visited={currentForm < prevForm}/>,
+        <Form2 onBack={onBack} onNext={onNext} key={1} visited={currentForm < prevForm}/>,
+        <Form3 onBack={onBack} key={2} visited={currentForm < prevForm}/>
+    ];
 
-        <div class='container first mx-auto my-auto bg-amber-200 flex flex-col'>
-          <div class='container p-12 '>REGISTRATION FORM</div>
-          <div class='flex p-8 bg-blue-500'>
-          <label class="block w-1/3 p-4 bg-green-200">
-            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-              Email
-            </span>
-            <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
-          </label>
-          <label class="block w-1/3 p-4 bg-green-200 ">
-            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-              Username
-            </span>
-            <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="@username" />
-          </label>
-          <label class="block w-1/3 p-4 bg-green-200">
-            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-             Password
-            </span>
-            <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="*******" />
-          </label>
-          </div>
+    return (<div>
+        <Head>
+            <title>Cfern</title>
+            <meta name="description" content="hello"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <div className={styles.main}>
+            {FORMS[currentForm]}
         </div>
-
-      </div>
-
-      </body>
-
-     
-  
-    </div>
-  )
+    </div>)
 }
