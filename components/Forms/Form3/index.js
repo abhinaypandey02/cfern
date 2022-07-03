3e06ecc974cddac60cbf9a94c4923c611dc252a1
Abhinay Pandey <abhinaypandey02@gmail.com>
2022-07-03 23:09:13 +0530

    validation

--- components/Forms/Form3/index.js
+++ components/Forms/Form3/index.js
@@ -3,39 +3,54 @@ import {useForm} from "react-hook-form";
 import ErrorMessageComponent from "../../../helpers/errorMessageComponent";
 import Tooltip from "../../Tooltip";
 import {PROVINCES} from '../../../helpers/values.json';
-export default function Form3({onNext,onBack,formData,visited}){
-    const {register, handleSubmit, formState:{errors:e}}=useForm({defaultValues:formData});
-    const formContainerStyle=visited?styles.backAnim:styles.nextAnim;
+import codes from '../../../config/country-codes';
+import axios from "axios";
+import {useState} from "react";
+
+export default function Form3({onNext, onBack, formData, visited}) {
+    const {register, handleSubmit, formState: {errors: e}, getValues, trigger} = useForm({defaultValues: formData});
+    const [loading, setLoading] = useState(false);
+    const formContainerStyle = visited ? styles.backAnim : styles.nextAnim;
+
     function onSubmit(data) {
         onNext(data)
     }
 
-    return <div tabIndex={0} className={formContainerStyle } >
-        <p className="font-bold text-2xl font">Canadian Mailing Address <Tooltip text={'If your address is outside Canada, you aren\'t able to use Wealthsimple Tax because of a NETFILE restriction.'}/> </p>
+    return <div tabIndex={0} className={formContainerStyle}>
+        <p className="font-bold text-2xl font">Canadian Mailing Address <Tooltip
+            text={'If your address is outside Canada, you aren\'t able to use Wealthsimple Tax because of a NETFILE restriction.'}/>
+        </p>
         <form noValidate={true} onSubmit={handleSubmit(onSubmit)}>
             <div className={styles.formGroup}>
                 <div className={styles.inputGroup}>
-                    <input type={'text'} {...register('co')}   placeholder={'C/O (Care of)'}/>
+                    <input type={'text'} {...register('co', {
+                        validate: {
+                            equalsAboutUsName: v => v === formData.firstName
+                        }
+                    })} placeholder={'C/O (Care of)'}/>
                 </div>
+                <ErrorMessageComponent e={e['co']}/>
+
                 <div className={styles.inputGroup}>
-                    <input type={'text'} {...register('unit')}   placeholder={'Unit'}/>
+                    <input type={'text'} {...register('unit')} placeholder={'Unit'}/>
                     {" - "}
-                    <input type={'text'} {...register('streetNo')}   placeholder={'Street Number'}/>
-                    <input type={'text'} {...register('streetName',{required:true})}   placeholder={'Street Name *'}/>
+                    <input type={'text'} {...register('streetNo')} placeholder={'Street Number'}/>
+                    <input type={'text'} {...register('streetName', {required: true})} placeholder={'Street Name *'}/>
                     <Tooltip text={"If you have a PO Box or RR number, enter it in the “Street name” field."}/>
                 </div>
-                <ErrorMessageComponent e={e['unit']||e['streetNo']||e['streetName']}/>
+                <ErrorMessageComponent e={e['unit'] || e['streetNo'] || e['streetName']}/>
 
                 <div className={styles.inputGroup}>
-                    <input type={'text'} {...register('city',{required:true})}   placeholder={'City *'}/>
+                    <input type={'text'} {...register('city', {required: true})} placeholder={'City *'}/>
 
-                    <select className={'flex-grow'} {...register('prov',{validate:{notBlank:v=>v!==''}})}   placeholder={'Province'}>
+                    <select className={'flex-grow'} {...register('prov', {validate: {notBlank: v => v !== ''}})}
+                            placeholder={'Province'}>
                         <option value={''}>Province or Territory *</option>
-                        {PROVINCES.map(p=><option key={p} value={p}>{p}</option>)}
+                        {PROVINCES.map(p => <option key={p} value={p}>{p}</option>)}
                     </select>
-                    <input type={'text'} {...register('postalCode',{required:true})}   placeholder={'Postal Code *'}/>
+                    <input type={'text'} {...register('postalCode', {required: true})} placeholder={'Postal Code *'}/>
                 </div>
-                <ErrorMessageComponent e={e['city']||e['prov']||e['postalCode']}/>
+                <ErrorMessageComponent e={e['city'] || e['prov'] || e['postalCode']}/>
 
             </div>
             <div className={styles.formGroup}>
@@ -43,16 +58,50 @@ export default function Form3({onNext,onBack,formData,visited}){
                     Home telephone number
                 </div>
                 <div className={styles.inputGroup}>
-                    <input type={'number'} {...register('phoneCode')}   placeholder={''}/>
-                    <input type={'number'} {...register('phoneNumber')}   placeholder={''}/>
+                    <input placeholder={'Area Code'} style={{width: 120}} list={'country-list'} {...register('phoneCode', {
+                        required: true, validate: {
+                            validCode: v => codes.find(ele => ele.MobileCode === v) !== undefined
+                        }
+                    })} id="country-codes"/>
+                    <datalist id={'country-list'}>
+                        <option value={""}/>
+                        {codes.map((code, index) => <option key={index} value={code.MobileCode}>{code.Name}</option>)}
+                    </datalist>
+                    <input type={'number'} {...register('phoneNumber', {
+                        validate: {
+                            validateNumberAPI: async v => {
+                                const noErrors = await trigger(['co','unit','streetNo','streetName','city','prov','postalCode','phoneCode']);
+                                if (!noErrors) return true;
+                                let code = getValues('phoneCode')
+                                if (!code) return false;
+                                setLoading(true);
+                                try{
+                                    const res = await axios.get(`https://api.apilayer.com/number_verification/validate?number=${code.slice(1) + v}`, {
+                                        headers: {
+                                            "apikey": "OjZjNzcNBEyO8xvVjilnHX5MjeBvC5Q5"
+                                        }
+                                    })
+                                    setLoading(false);
+                                    return res.data.valid;
+
+                                } catch(e){
+                                    setLoading(false);
+
+                                    return true;
+                                }
+
+                            }
+                        }
+                    })} placeholder={'Phone Number'}/>
                 </div>
-                <ErrorMessageComponent e={e['phoneCode']||e['phoneNumber']}/>
+                <ErrorMessageComponent e={e['phoneCode'] || e['phoneNumber']}/>
 
             </div>
             <div className={styles.formGroup}>
                 <div className={styles.inputGroup}>
-                    <button onClick={onBack} className={styles.btnBack} >Back</button>
-                    <button type={'submit'} className={styles.btnNext} >Next</button>
+                    <button onClick={onBack} className={styles.btnBack}>Back</button>
+                    <button type={'submit'} disabled={loading}
+                            className={styles.btnNext}>{loading ? "Validating number" : "Next"}</button>
                 </div>
 
             </div>
